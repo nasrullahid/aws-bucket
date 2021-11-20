fa6d547a709d304c9832db37b9b4163b425b995d
Nasrullah <nasrullah_x2@telkomsel.co.id>
2021-11-20 15:10:08 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const aws = require('aws-sdk')
+const multer = require('multer')
+const multerS3 = require('multer-s3')
+
+dotenv.config()
+const app = express()
+// middleWare
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+
+aws.config.update({
+  apiVersion: '2006-03-01',
+  accessKeyId: process.env.AWSAccessKeyId,
+  secretAccessKey: process.env.AWSSecretKey,
+  region: process.env.AWSRegion
+})
+
+const s3 = new aws.S3()
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.AWSBucket,
+    acl: 'public-read',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: file.fieldname })
+    },
+    key: (req, file, cb) => {
+      cb(null, Date.now().toString() + file.originalname)
+    }
+  })
+})
+
+app.get('/', async (req, res) => {
+  res.status(200).send('S3 Upload Backend')
+})
+
+app.post('/', upload.single('photo'), async (req, res) => {
+  console.log(req.file)
+  try {
+    res.status(201).json({ file: req.file.location })
+  } catch (err) {
+    res.status(500).send({
+      error: 'error occured creating user'
+    })
+  }
+})
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+  console.log(`server started on port ${port}`)
+})
